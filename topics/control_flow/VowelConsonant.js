37f8b3a8e5571556970c64f8048f91bf0f27b0e1
gaurav <payme4work17461@gmail.com>
2025-02-19 01:46:34 +0530

    Vowel or Consonant-modified

--- topics/control_flow/VowelConsonant.js
+++ topics/control_flow/VowelConsonant.js
@@ -2,7 +2,7 @@
 //as input and returns "vowel" or "consonant". (Handle uppercase and lowercase).
 
 function checkVowelConsonant(char){
-    if(char === "a" || char === "e" || char === "i" || char === "o" || char === "u"){
+    if(char === "a" || char === "e" || char === "i" || char === "o" || char === "u" || char === "A" || char === "E" || char === "I" || char === "O" || char === "U"){
         return "Vowel";
     }else{
         return "Consonant";
@@ -13,3 +13,6 @@ console.log(checkVowelConsonant("a"));
 console.log(checkVowelConsonant("p"));
 console.log(checkVowelConsonant("z"));
 console.log(checkVowelConsonant("u"));
+console.log(checkVowelConsonant("A"));
+console.log(checkVowelConsonant("Y"));
+console.log(checkVowelConsonant("O"));
