cdd0ca3c52f5c3720a2492b0a831f28fb2e250d5
gaurav <payme4work17461@gmail.com>
2025-02-24 13:59:28 +0530

    DoWhile

---
+++ topics/loops/DoWhile.js
@@ -0,0 +1,8 @@
+//Do...While Example: Write a program that uses a do...while loop to repeatedly ask the user
+//for a number until they enter a positive number.
+
+let i = -15;
+do {
+  console.log("The number is " + i);
+  i++;
+} while (i < 1);
