e4b0836665ebfc3e61b49413183d2afe87e5cd56
gaurav <payme4work17461@gmail.com>
2025-02-24 14:11:02 +0530

    greet using function

---
+++ topics/functions/GreetingFunction.js
@@ -0,0 +1,7 @@
+//Greeting Function: Write a function that takes a name as input and returns
+//a greeting message (e.g., "Hello, [name]!").
+
+function greet(name){
+    console.log(`Hello, ${name}`);
+}
+greet("javascript!!");
